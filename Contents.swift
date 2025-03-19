15a9cd5a9e8a59278171284a9304fda30698f564
Edwin Cardenas <ecardenas@lwolf.com>
2025-03-18 22:31:10 -0500

    Initial commit

---
+++ Contents.swift
@@ -0,0 +1,47 @@
+import Cocoa
+
+let playground = "Hello, playground"
+var mutablePlayground = "Hello, mutable playground"
+
+mutablePlayground += "!"
+
+//let quote = "I wanted to \"say\":\n\(playground)"
+let quote = #"I wanted to \"say\":\n\(playground)"# // raw string
+
+print(quote)
+
+for c: Character in mutablePlayground {
+    print("'\(c)'")
+}
+
+let snowman = "\u{2603}"
+print(snowman)
+
+let aAcute = "\u{0061}\u{0301}"
+print(aAcute)
+
+for scalar in playground.unicodeScalars {
+    print("\(scalar.value)")
+}
+
+let aACutePrecomposed = "\u{00E1}"
+print(aACutePrecomposed)
+
+let b = (aAcute == aACutePrecomposed)
+print(b)
+
+aAcute.count
+aACutePrecomposed.count
+
+let singleCharacter: Character = "a" // type annotation was required, otherwise it would create a string with count == 1
+
+let start = playground.startIndex
+let end = playground.index(start, offsetBy: 4)
+let fifthCharacter = playground[end] // yields "o"
+
+let range: ClosedRange<String.Index> = start...end // closed range
+let firstFiveChars = playground[range]
+
+//let anotherRange: PartialRangeThrough<String.Index> = ...end // one-sided range
+let anotherRange: PartialRangeFrom<String.Index> = end... // one-sided range
+let otherChars = playground[anotherRange]
